dc8f4e152c2f384fb19ad47416158bee6f0f7ddb
qrouet <quentinrouet@hotmail.fr>
2022-04-21 14:12:32 +0200

    DataBinding

--- mod4databinding/src/main/java/com/eni/mod4databinding/MainActivity.kt
+++ mod4databinding/src/main/java/com/eni/mod4databinding/MainActivity.kt
@@ -4,16 +4,22 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.eni.mod4databinding.databinding.ActivityMainBinding
+import io.github.serpro69.kfaker.faker
 
 class MainActivity : AppCompatActivity() {
     lateinit var amb: ActivityMainBinding;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         amb = DataBindingUtil.setContentView(this,R.layout.activity_main)
-        val manu : Utilisateur = Utilisateur("Macron","Manu","1977","H")
-        val valls : Utilisateur = Utilisateur("Valls","Manu","1962","H")
-        val elon : Utilisateur = Utilisateur("Musk","Elon","1971","H")
-        val sibeth : Utilisateur = Utilisateur("Ndiaye","Sibeth","1979","F")
+        val faker = faker {fakerConfig { locale="fr" }}
+        val user = Utilisateur(
+            faker.name.lastName(),
+            faker.name.firstName(),
+            (1900..2000).random().toString(),
+            faker.gender.types()
+        )
+        amb.utilisateur = user
 
+        amb.textViewGenre.textSize = 25.0f
     }
 }
