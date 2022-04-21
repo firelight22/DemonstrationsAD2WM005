2075fcf1582c0059a5f4c15c7fd197926f4953e8
qrouet <quentinrouet@hotmail.fr>
2022-04-21 08:57:50 +0200

    Buttons OnClickListener

--- mod4findviewbyid/src/main/java/com/eni/mod4findviewbyid/MainActivity.kt
+++ mod4findviewbyid/src/main/java/com/eni/mod4findviewbyid/MainActivity.kt
@@ -2,20 +2,32 @@ package com.eni.mod4findviewbyid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
-import android.widget.RatingBar
-import android.widget.TextView
+import android.view.View
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Récupération vue par l'id
         val textView = findViewById<TextView>(R.id.textView)
         val editText = findViewById<EditText>(R.id.editText)
         val ratingBar = findViewById<RatingBar>(R.id.ratingBar)
+        //Modification des attributs
         textView.text = "Hello"
         editText.hint = "Comment allez-vous?"
         editText.text.clear()
         ratingBar.rating = 3.0f
+
+        //Récupération des instances de mes boutons
+        val button = findViewById<Button>(R.id.button)
+        val button2 = findViewById<Button>(R.id.button2)
+        button.setOnClickListener {
+            Toast.makeText(this, "Hello tu as appuyé sur un bouton", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun onClickButton2(v:View){
+        Toast.makeText(this, "2 eme bouton", Toast.LENGTH_SHORT).show()
     }
 }
